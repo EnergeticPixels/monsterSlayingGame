58cf3e48e4f20209726d4f8732f01dee2ad1c18c
energeticpixels@gmail.com <energeticpixels@gmail.com>
2019-07-20 23:20:05 -0500

    configs log for other buttons

--- app.js
+++ app.js
@@ -11,14 +11,15 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
       var damage = this.calculateDamage(3, 10);
       this.monsterHealth -= damage;
       this.turns.unshift({
         isPlayer: true,
-        text: 'Player hits monsterfor ' + damage
-      })
+        text: 'Player hits Monster for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
@@ -26,7 +27,12 @@ new Vue({
       this.monsterAttacks();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster hard for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
@@ -38,6 +44,10 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits heals for 10'
+      });
       this.monsterAttacks();
     },
     giveUp: function() {
